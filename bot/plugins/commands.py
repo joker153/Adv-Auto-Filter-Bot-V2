64e6b2d05a91e0b3a1bc9fb91b903ed6f540d1d6
joker153 <77005994+joker153@users.noreply.github.com>
2021-11-15 11:43:36 +0530

    Add files via upload

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⚜️Channel⚜️', url="https://t.me/M5_UPATE"
                                 )
                         ]
                     ]
