b8021043b7e1815b24bf411ed05c7c620314e100
joker153 <77005994+joker153@users.noreply.github.com>
2021-11-15 11:11:06 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -42,7 +42,69 @@ For More Details Click Help Button Below..
             -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            #!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+# (c) @AlbertEinsteinTG
+
+
+
+class Translation(object):
+
+    
+
+    START_TEXT = """<b>Hey {}!!</b>
+
+<b>ഞാനൊരു അമാനുഷികനായ ബോട്ടാണ് ഞാൻ @MOVES_TELEGRAM ഗ്രൂപ്പിലേക്ക് സിനിമ എല്ലാം കൊടുക്കുന്നത് ഞാനാണ്😁</b>
+
+
+
+
+
+<b>ഇവിടെ ഇരുന്ന്  Start കുത്തി കളിക്കാതെ സ്ഥലം  വിട്ടോ😂</b>
+
+
+
+
+
+🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <b><i><a href="https://t.me/Rfxjoker">🅹🅾🅺🅴🆁</a></i></b>
+
+</i>"""    
+
+    
+
+    HELP_TEXT = """
+
+<b>നോക്കിയിരുന്നോ ഇപ്പൊ ഹെൽപ്പ് ചെയ്യും😁</b>
+
             
+
+ @movieuploaders
+
+"""
+
+    
+
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+
+    
+
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Rfxjoker">Rfxjoker</a></i></b>
+
+
+
+<b>➥ Language</b> : <code>Python3</code>
+
+
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+
+
+<b>➥ Source Code</b> : <i><a href="https://t.me/@Movieuploaders2">Click Me</a></i>
+
+"""
 @CrazyBotsz
 """
     
